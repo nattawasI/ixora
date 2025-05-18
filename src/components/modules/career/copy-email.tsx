1043fafd704b0bd379554cba9f44510e0e897634
nattawas.i <nattawas.i@gmail.com>
2025-05-18 09:57:28 +0700

    save

--- src/components/modules/career/copy-email.tsx
+++ src/components/modules/career/copy-email.tsx
@@ -4,7 +4,9 @@ import { useClipboard } from '@/libs/hooks/use-clipboard'
 import { ButtonCopy } from '@/components/ui/button-copy'
 
 const CopyEmail = ({ value }: { value: string }) => {
-  const { copy, copied } = useClipboard()
+  const { copy, copied } = useClipboard({
+    timeout: 1000,
+  })
 
   return <ButtonCopy isCopied={copied} onClick={() => copy(value)} />
 }
