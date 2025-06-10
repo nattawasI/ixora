416f51aaebe25ef7f23b51af7a0264603f5f5c7c
nattawas.i <nattawas.i@gmail.com>
2025-06-11 02:17:30 +0700

    explore-more use Collapsible

--- src/components/modules/project-detail/project-explore-more.tsx
+++ src/components/modules/project-detail/project-explore-more.tsx
@@ -1,11 +1,11 @@
-import { ExploreMoreAccordion } from '@/components/modules/article-detail/explore-more-accordion'
+import { ExploreMoreCollapsible } from '@/components/modules/article-detail/explore-more-collapsible'
 import { ProjectCard } from '@/components/modules/projects/project-card'
 import Link from 'next/link'
 import { cn } from '@/libs/utils/cn'
 
 const ProjectExploreMore = ({ isInModal }: { isInModal?: boolean }) => {
   return (
-    <ExploreMoreAccordion type="single" collapsible>
+    <ExploreMoreCollapsible>
       <div className={cn('grid gap-2.5 max-lg:pt-4 md:grid-cols-3', isInModal ? 'lg:pb-10' : 'md:pb-7.5')}>
         {Array.from({ length: 3 }).map((_, index) => (
           <Link href="/projects/residential/4" className="block" key={index}>
@@ -24,7 +24,7 @@ const ProjectExploreMore = ({ isInModal }: { isInModal?: boolean }) => {
           </Link>
         ))}
       </div>
-    </ExploreMoreAccordion>
+    </ExploreMoreCollapsible>
   )
 }
 
