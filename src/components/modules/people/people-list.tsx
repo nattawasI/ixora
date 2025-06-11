8aeebff92212a31060cfdb7fc954d2a70f5a0c7f
Prapat Prapatsornmanu <prapat.psm@gmail.com>
2025-06-12 01:17:22 +0700

    save integrate people

--- src/components/modules/people/people-list.tsx
+++ src/components/modules/people/people-list.tsx
@@ -1,15 +1,47 @@
 import { CardPeople, CardPeopleLoading } from '@/components/ui/card-people'
 
+/** directus */
+import { directus } from '@/libs/directus'
+import { PeopleResponse } from '@/libs/directus/type'
+import { readItems } from '@directus/sdk'
+
 const PeopleList = async () => {
   /** fetch here */
+  const data = await directus.request<PeopleResponse[]>(
+    readItems('people', {
+      filter: {
+        status: {
+          _eq: 'published',
+        },
+      },
+      fields: [
+        'id',
+        'status',
+        'first_name',
+        'last_name',
+        'position',
+        'image.id',
+        'image.filename_disk',
+        'image.filename_download',
+        'image.width',
+        'image.height',
+        'image.filesize',
+        'image.title',
+      ],
+    }),
+  )
+
   return (
     <div className="list-people">
-      {Array.from({ length: 8 }).map((_, index) => (
+      {data?.map((item, index) => (
         <CardPeople
           key={index}
-          image={{ src: '/mockup/people.jpg', alt: 'Chanchai Jarungruangkiat' }}
-          name={'Chanchai Jarungruangkiat'}
-          position="Founder / Partner"
+          image={{
+            src: `${process.env.DIRECTUS_URL}/assets/${item.image?.id}`,
+            alt: `${item.first_name} ${item.last_name}`,
+          }}
+          name={`${item.first_name} ${item.last_name}`}
+          position={item.position}
         />
       ))}
     </div>
--- src/libs/directus/index.ts
+++ src/libs/directus/index.ts
@@ -3,22 +3,22 @@ import 'server-only'
 import { createDirectus, rest, staticToken } from '@directus/sdk'
 
 // ตรวจสอบว่า environment variables ถูกตั้งค่าหรือไม่
-if (!process.env.NEXT_PUBLIC_DIRECTUS_URL) {
-  throw new Error('NEXT_PUBLIC_DIRECTUS_URL is not defined in environment variables.')
+if (!process.env.DIRECTUS_URL) {
+  throw new Error('DIRECTUS_URL is not defined in environment variables.')
 }
-if (!process.env.NEXT_PUBLIC_DIRECTUS_API_KEY) {
-  throw new Error('NEXT_PUBLIC_DIRECTUS_API_KEY is not defined in environment variables.')
+if (!process.env.DIRECTUS_API_KEY) {
+  throw new Error('DIRECTUS_API_KEY is not defined in environment variables.')
 }
 
 // สร้าง Directus Client instance
 // ใช้ staticToken เพื่อยืนยันตัวตน
-// `process.env.NEXT_PUBLIC_DIRECTUS_URL` จะเข้าถึงได้เฉพาะฝั่ง server เท่านั้น
-const directus = createDirectus(process.env.NEXT_PUBLIC_DIRECTUS_URL)
+// `process.env.DIRECTUS_URL` จะเข้าถึงได้เฉพาะฝั่ง server เท่านั้น
+const directus = createDirectus(process.env.DIRECTUS_URL)
   .with(
     rest({
       onRequest: (opts) => ({ ...opts, cache: 'no-store' }),
     }),
   )
-  .with(staticToken(process.env.NEXT_PUBLIC_DIRECTUS_API_KEY))
+  .with(staticToken(process.env.DIRECTUS_API_KEY))
 
 export { directus }
