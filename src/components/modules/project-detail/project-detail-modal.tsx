5bb822e7a4fd3335ff2975037fa93f813964481f
nattawas.i <nattawas.i@gmail.com>
2025-06-17 23:59:19 +0700

    save task

--- src/components/modules/project-detail/project-detail-modal.tsx
+++ src/components/modules/project-detail/project-detail-modal.tsx
@@ -1,22 +1,22 @@
 'use client'
 
 import { useRouter } from 'next/navigation'
-import { PageDetailModal, PageDetailModalContent } from '@/components/ui/page-modal'
+import { PageModal, PageModalContent } from '@/components/ui/page-modal'
 
 const ProjectDetailModal = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter()
 
   return (
-    <PageDetailModal
+    <PageModal
       defaultOpen
       onOpenChange={() => {
         router.back() // logic ตรงนี้อาจจะเปลี่ยนทีหลัง เพราะ modal เรามี prev, next ทำให้ไม่สามารถใช้ router.back() ได้
       }}
     >
-      <PageDetailModalContent aria-describedby={undefined} contentSize="large">
+      <PageModalContent aria-describedby={undefined} contentSize="large">
         {children}
-      </PageDetailModalContent>
-    </PageDetailModal>
+      </PageModalContent>
+    </PageModal>
   )
 }
 
