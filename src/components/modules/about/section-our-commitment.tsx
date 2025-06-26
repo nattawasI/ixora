3219928d7cbd4050c479f6347d4b174574b4906f
Prapat Prapatsornmanu <prapat.psm@gmail.com>
2025-06-27 01:39:06 +0700

    fix style

--- src/components/modules/about/section-our-commitment.tsx
+++ src/components/modules/about/section-our-commitment.tsx
@@ -1,20 +1,24 @@
 import { ButtonArrow } from '@/components/ui/button-arrow'
+import Link from 'next/link'
 
 const SlideOurCommitment = () => {
   return (
     <div className="c-container about-page-container space-y-16">
-      <div className="space-y-5">
-        <h2 className="typo-title-2 font-bold">OUR COMMITMENT</h2>
-        <p className="typo-title-3">
-          Our commitment to excellence is recognized through numerous prestigious awards, including the TALA Design
-          Award, Muse Design Award, and the esteemed German Design Award.
-          <br />
-          <br />
-          These accolades highlight our dedication to pushing the boundaries of landscape architecture.
-        </p>
+      <div className="grid grid-cols-12 gap-y-5">
+        <div className="col-span-12 space-y-5 md:col-span-9 md:col-start-4">
+          <h2 className="typo-title-2 font-bold">OUR COMMITMENT</h2>
+          <p className="md:typo-title-3 typo-body-2">
+            Our commitment to excellence is recognized through numerous prestigious awards, including the TALA Design
+            Award, Muse Design Award, and the esteemed German Design Award.
+            <br />
+            <br />
+            These accolades highlight our dedication to pushing the boundaries of landscape architecture.
+          </p>
+        </div>
+        <Link href="/awards" className="col-span-12 md:col-span-9 md:col-start-4">
+          <ButtonArrow>Award & Press</ButtonArrow>
+        </Link>
       </div>
-
-      <ButtonArrow>Award & Press</ButtonArrow>
     </div>
   )
 }
