6a2e768550e9ce78e68517f969e7cae2f4caeec6
nattawas.i <nattawas.i@gmail.com>
2025-06-28 22:26:01 +0700

    save task

--- src/components/modules/about/section-who-we-are.tsx
+++ src/components/modules/about/section-who-we-are.tsx
@@ -18,6 +18,8 @@ const SectionWhoWeAre = ({ isDesktop }: { isDesktop?: boolean }) => {
     const contentMoreEl = contentMoreRef.current
 
     if (contentRootEl && contentMoreEl && isDesktop) {
+      // Reset height to auto to get the natural height
+      contentRootEl.style.height = 'auto'
       setWrapperHeight(contentRootEl.scrollHeight + contentMoreEl.scrollHeight)
     }
   }, [isDesktop])
@@ -49,46 +51,48 @@ const SectionWhoWeAre = ({ isDesktop }: { isDesktop?: boolean }) => {
   return (
     <div className="c-container about-page-container relative">
       <div className="grid grid-cols-12">
-        <div className="col-span-12 space-y-7 md:col-span-9 md:col-start-4" style={{ height: wrapperHeight }}>
-          <div ref={contentRootRef}>
-            <div className="space-y-5">
-              <h2 className="typo-title-2 font-bold">WHO WE AREA</h2>
-              <h3 className="typo-body-2 md:typo-title-3">
-                We are dedicated to exploring the limitless possibilities of landscape architectural design, crafting
-                spaces that seamlessly integrate the beauty of nature with human needs.
-              </h3>
-            </div>
-            <ReadMoreBlock
-              contentLead={
-                <p>
-                  <span className="font-bold">IXORA DESIGN</span> was founded in 2003, Ixora Design emerged from the
-                  collaborative vision of four seasoned landscape architects with a wealth of international experience
-                  spanning Thailand, Asia, Australia, and the United States. Driven by a passion to elevate the
-                  profession, our dynamic team is committed to delivering innovative and
-                  <span className="font-bold">sustainable landscape solutions</span>
-                </p>
-              }
-              contentMoreRef={contentMoreRef}
-              contentMore={
-                <p>
-                  Over two decades, Ixora Design has not only delivered commercially successful projects but has also
-                  cultivated
-                  <span className="font-bold">a deep expertise in sustainable design</span>. Through experimental
-                  projects, design competitions, and collaborations with educational, public, and private sectors, we
-                  translate ecological design theories into practical, sustainable and impactful solutions. We are
-                  dedicated to environmental preservation and restoring balance to natural ecosystems. Ixora Design
-                  partners with clients to create exceptional landscapes that seamlessly integrate function, technology,
-                  and aesthetic design with the natural environment. We are committed to fostering a sustainable future
-                  for people, animals, and the planet.
-                </p>
-              }
-              open={open}
-              onOpenChange={setOpen}
-              onAnimationComplete={() => {
-                swiper.updateAutoHeight()
-              }}
-            />
+        <div
+          className="col-span-12 space-y-7 md:col-span-9 md:col-start-4"
+          ref={contentRootRef}
+          style={{ height: wrapperHeight }}
+        >
+          <div className="space-y-5">
+            <h2 className="typo-title-2 font-bold">WHO WE AREA</h2>
+            <h3 className="typo-body-2 md:typo-title-3">
+              We are dedicated to exploring the limitless possibilities of landscape architectural design, crafting
+              spaces that seamlessly integrate the beauty of nature with human needs.
+            </h3>
           </div>
+          <ReadMoreBlock
+            contentLead={
+              <p>
+                <span className="font-bold">IXORA DESIGN</span> was founded in 2003, Ixora Design emerged from the
+                collaborative vision of four seasoned landscape architects with a wealth of international experience
+                spanning Thailand, Asia, Australia, and the United States. Driven by a passion to elevate the
+                profession, our dynamic team is committed to delivering innovative and&nbsp;
+                <span className="font-bold">sustainable landscape solutions</span>
+              </p>
+            }
+            contentMoreRef={contentMoreRef}
+            contentMore={
+              <p>
+                Over two decades, Ixora Design has not only delivered commercially successful projects but has also
+                cultivated&nbsp;
+                <span className="font-bold">a deep expertise in sustainable design</span>. Through experimental
+                projects, design competitions, and collaborations with educational, public, and private sectors, we
+                translate ecological design theories into practical, sustainable and impactful solutions. We are
+                dedicated to environmental preservation and restoring balance to natural ecosystems. Ixora Design
+                partners with clients to create exceptional landscapes that seamlessly integrate function, technology,
+                and aesthetic design with the natural environment. We are committed to fostering a sustainable future
+                for people, animals, and the planet.
+              </p>
+            }
+            open={open}
+            onOpenChange={setOpen}
+            onAnimationComplete={() => {
+              swiper.updateAutoHeight()
+            }}
+          />
         </div>
       </div>
     </div>
